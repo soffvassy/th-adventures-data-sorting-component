a8a847aa265c74bfaa990254ce6351ce33bde4b3
Soff Vassy <soffvassy@users.noreply.github.com>
2024-10-31 00:17:25 +0200

    Initial commit

---
+++ data-sorting-component/README.md
@@ -0,0 +1,46 @@
+## Treehouse Code Adventures: Data Sorting Component
+
+Giving the end-user multiple ways to sort/view their data is a great UX feature. This can be relatively easily by thinking ahead and writing some good CSS. Of course, minimial javascript as well.
+
+Create a component that will allow users to see the exact same data in a minimum of 2 ways. As per the mockups; card view and list view.
+
+Use vanilla javascript or any framework you'd like to tackle this Code Adventure!
+
+---
+
+#### Font Family: [Inter](https://fonts.google.com/specimen/Inter)
+
+#### Color Palette:
+
+| Name                | Hex Color |
+| ------------------- | :-------: |
+| Component Primary   | `#222527` |
+| Component Secondary | `#313639` |
+| Text Primary        | `#FFFFFF` |
+| Text Secondary      | `#787C7F` |
+| ------------------- | :-------: |
+
+| Data Color Options | Hex Color |
+| ------------------ | :-------: |
+| Option1            | `#6EB1E4` |
+| Option2            | `#E4AE6E` |
+| Option3            | `#BEE46E` |
+| Option4            | `#CC6EE4` |
+| Option5            | `#6EE4E4` |
+| Option6            | `#936EE4` |
+| Option7            | `#E46E9C` |
+| Option8            | `#6EE479` |
+
+#### How to Succeed
+
+1. Create the UI as closely to the mockups as possible.
+
+2. Be sure to set up a "default view" with the corresponding icon having an active state.
+
+3. Upon clicking on a different view, active states should update respectively.
+
+4. Data should sort according to the active icon.
+
+#### Extra Challenges
+
+1. Every item (card, list, etc) should have an animation.
---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+import data from './data.json'
+
+function App() {
+  const datadrop=data;
+  const [searchValue, setSearchValue] = useState('');
+  const [statusValue, setStatusValue] = useState('');
+  const [view, setView] = useState('grid');
+  const Item = ({title, description, color, statuss}) => {
+    
+    return(
+      <div className="container">
+        <div className="container-inner">
+        <div style={{backgroundColor: color}}></div> 
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <span className={statuss=="completed" ? "green" : statuss=="in progress" ? "yellow" : "grey"}>{statuss}</span>
+      </div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+    <h1>Data Sorting Component</h1>
+    <div className={view + " component"}>
+      <input className="search"
+      type="text"
+      placeholder='Search'
+      value={searchValue}
+      onChange={(e)=>setSearchValue(e.target.value)}
+      />
+      <button className={view==="grid" ? "active" : ""} value="grid" onClick={(e)=>setView(e.target.value.toLowerCase())}>Grid</button>
+      <button className={view==="list" ? "active" : ""} value="list" onClick={(e)=>setView(e.target.value.toLowerCase())}>List</button>
+      <div className="statuses">
+        <button className={statusValue==="completed" ? "active" : ""} value="completed" onClick={(e)=>setStatusValue(e.target.value)}>completed</button>
+        <button className={statusValue==="in progress" ? "active" : ""} value="in progress" onClick={(e)=>setStatusValue(e.target.value)}>in progress</button>
+        <button className={statusValue==="not started" ? "active" : ""} value="not started" onClick={(e)=>setStatusValue(e.target.value)}>not started</button>
+        <button value="" onClick={(e)=>setStatusValue("")}>Clear</button>
+        </div>
+      <div className="wrap-container">
+      {datadrop
+      .filter(item=> item.title.toLowerCase().includes(searchValue.toLowerCase()) || item.description.toLowerCase().includes(searchValue.toLowerCase()))
+      .filter(item=>item.status.includes(statusValue))
+      .map((item) => (
+        <Item 
+          key={item.title}
+          title={item.title}
+          color={item.color}
+          description={item.description}
+          statuss={item.status}
+        />
+      ))}
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
